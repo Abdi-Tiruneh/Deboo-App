c6b3283469e64ec8d3947d0415e87d5547fc86e6
Cooperative Bank of Oromia <99472505+Abdi-Tiruneh@users.noreply.github.com>
2023-07-13 10:46:26 +0300

    Update ApplicationExceptionHandler.java

--- src/main/java/com/dxvalley/crowdfunding/exception/handler/ApplicationExceptionHandler.java
+++ src/main/java/com/dxvalley/crowdfunding/exception/handler/ApplicationExceptionHandler.java
@@ -2,12 +2,12 @@ package com.dxvalley.crowdfunding.exception.handler;
 
 import com.dxvalley.crowdfunding.exception.customException.*;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,118 +20,79 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-@RequiredArgsConstructor
 @Slf4j
 public class ApplicationExceptionHandler {
     private final DateTimeFormatter dateTimeFormatter;
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
-    public ResponseEntity<Object> handleInvalidArgument(Exception ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        if (ex instanceof MethodArgumentNotValidException) {
-            MethodArgumentNotValidException methodEx = (MethodArgumentNotValidException) ex;
-            methodEx.getBindingResult().getFieldErrors().forEach((error) -> {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            });
-        }
+    public ApplicationExceptionHandler(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
+    }
 
-        BindException bindEx = (BindException) ex;
-        bindEx.getBindingResult().getFieldErrors().forEach((error) -> {
-            errorMap.put(error.getField(), error.getDefaultMessage());
-        });
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = processFieldErrors(ex.getBindingResult());
+        return ResponseEntity.badRequest().body(errorMap);
+    }
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Map<String, String>> handleBindException(BindException ex) {
+        Map<String, String> errorMap = processFieldErrors(ex.getBindingResult());
         return ResponseEntity.badRequest().body(errorMap);
     }
 
     @ExceptionHandler({BadRequestException.class, MultipartException.class, HttpRequestMethodNotSupportedException.class})
-    public ResponseEntity<Object> handleBadRequestException(Exception ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                ex.getMessage(),
-                request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(apiException);
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.BAD_REQUEST);
     }
 
-//    @ExceptionHandler({UnauthorizedException.class,BannedUserException.class})
-//    public ResponseEntity<Object> handleUnauthorizedException(Exception ex, HttpServletRequest request) {
-//        HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
-//        ExceptionResponse apiException = new ExceptionResponse(
-//                LocalDateTime.now().format(dateTimeFormatter),
-//                httpStatus,
-//                ex.getMessage(),
-//                request.getRequestURI()
-//        );
-//        return ResponseEntity.status(httpStatus).body(apiException);
-//    }
-
-    @ExceptionHandler({InternalAuthenticationServiceException.class})
-    public ResponseEntity<Object> handleBannedUserException(InternalAuthenticationServiceException ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                ex.getMessage(),
-                request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(apiException);
+    @ExceptionHandler({UnauthorizedException.class, BannedUserException.class})
+    public ResponseEntity<ExceptionResponse> handleUnauthorizedException(Exception ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.UNAUTHORIZED);
     }
 
-    @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                ex.getMessage(),
-                request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(apiException);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({ResourceAlreadyExistsException.class})
-    public ResponseEntity<Object> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.CONFLICT;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                ex.getMessage(),
-                request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(apiException);
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.CONFLICT);
     }
-    @ExceptionHandler({ForbiddenException.class})
-    public ResponseEntity<Object> handleForbiddenException(ForbiddenException ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ExceptionResponse> handleForbiddenException(ForbiddenException ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(PaymentCannotProcessedException.class)
+    public ResponseEntity<ExceptionResponse> handlePaymentCannotProcessedException(PaymentCannotProcessedException ex, HttpServletRequest request) {
+        return buildResponse(ex.getMessage(), request, HttpStatus.PAYMENT_REQUIRED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception ex, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorMessage = "An unexpected error occurred while processing your request. Please try again later or contact support.";
+        log.error("INTERNAL_SERVER_ERROR: " + ex.getMessage());
+        return buildResponse(errorMessage, request, httpStatus);
+    }
+
+    private ResponseEntity<ExceptionResponse> buildResponse(String errorMessage, HttpServletRequest request, HttpStatus httpStatus) {
         ExceptionResponse apiException = new ExceptionResponse(
                 LocalDateTime.now().format(dateTimeFormatter),
                 httpStatus,
-                ex.getMessage(),
+                errorMessage,
                 request.getRequestURI()
         );
         return ResponseEntity.status(httpStatus).body(apiException);
     }
 
-    @ExceptionHandler({PaymentCannotProcessedException.class})
-    public ResponseEntity<Object> handlePaymentCannotProcessedException(PaymentCannotProcessedException ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.PAYMENT_REQUIRED;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                ex.getMessage(),
-                request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(apiException);
-    }
-
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<ExceptionResponse> handleException(Exception ex, HttpServletRequest request) {
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        ExceptionResponse apiException = new ExceptionResponse(
-                LocalDateTime.now().format(dateTimeFormatter),
-                httpStatus,
-                "An unexpected error occurred while processing your request. Please try again later or contact support.",
-                request.getRequestURI());
-        log.error("INTERNAL_SERVER_ERROR: " + ex.getMessage());
-        return ResponseEntity.status(httpStatus).body(apiException);
+    private Map<String, String> processFieldErrors(BindingResult bindingResult) {
+        Map<String, String> errorMap = new HashMap<>();
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+        return errorMap;
     }
 }
-
-
