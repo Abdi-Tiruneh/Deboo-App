0cbd07e83f20ffe8c7239c0ef108f63ba7ac284e
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-07-15 11:11:08 +0300

    add resubmit option for campaign approvals

--- src/main/java/com/dxvalley/crowdfunding/admin/campaign/AdminCampaignController.java
+++ src/main/java/com/dxvalley/crowdfunding/admin/campaign/AdminCampaignController.java
@@ -1,14 +1,15 @@
 package com.dxvalley.crowdfunding.admin.campaign;
 
-import com.dxvalley.crowdfunding.campaign.campaign.Campaign;
 import com.dxvalley.crowdfunding.campaign.campaign.CampaignOperationService;
 import com.dxvalley.crowdfunding.campaign.campaign.CampaignRetrievalService;
 import com.dxvalley.crowdfunding.campaign.campaign.dto.CampaignDTO;
 import com.dxvalley.crowdfunding.campaign.campaignApproval.dto.ApprovalResponse;
 import com.dxvalley.crowdfunding.campaign.campaignApproval.dto.CampaignApprovalReq;
 import com.dxvalley.crowdfunding.utils.ApiResponse;
+import com.dxvalley.crowdfunding.utils.PaginationUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,73 +26,101 @@ public class AdminCampaignController {
     private final AdminCampaignService adminCampaignService;
     private final AdminCampaignApprovalService adminCampaignApprovalService;
 
-    @GetMapping({"/approvals/{campaignId}"})
-    public ResponseEntity<ApprovalResponse> getCampaignApprovalByCampaignId(@PathVariable Long campaignId) {
-        return ResponseEntity.ok(this.adminCampaignApprovalService.getCampaignApprovalByCampaignId(campaignId));
-    }
-
-    @PostMapping({"/approvals"})
-    public ResponseEntity<String> approveCampaign(@ModelAttribute @Valid CampaignApprovalReq campaignApprovalReq) {
-        return this.adminCampaignApprovalService.approveCampaign(campaignApprovalReq);
-    }
-
     @GetMapping
-    ResponseEntity<List<Campaign>> getCampaigns() {
-        return ResponseEntity.ok(this.adminCampaignService.getCampaigns());
+    public ResponseEntity<List<CampaignDTO>> getCampaigns(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection) {
+
+        Pageable pageable = PaginationUtils.createPageableWithSorting(page, size, sortBy, sortDirection);
+        return campaignRetrievalService.getCampaigns(pageable);
     }
 
-    @GetMapping({"campaignId/{campaignId}"})
-    ResponseEntity<CampaignDTO> getCampaign(@PathVariable Long campaignId) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignById(campaignId));
+    @GetMapping({"/{id}"})
+    ResponseEntity<CampaignDTO> getCampaign(@PathVariable Long id) {
+        return ResponseEntity.ok(campaignRetrievalService.getCampaignById(id));
+    }
+    @GetMapping({"/owner/{username}"})
+    ResponseEntity<List<CampaignDTO>> getCampaignByOwner(@PathVariable String username) {
+        return ResponseEntity.ok(campaignRetrievalService.getCampaignsByOwner(username));
     }
 
-    @GetMapping({"/campaignOwner/{owner}"})
-    ResponseEntity<List<CampaignDTO>> getCampaignByOwner(@PathVariable String owner) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignsByOwner(owner));
+    @GetMapping({"/stage/{stage}"})
+    ResponseEntity<List<CampaignDTO>> getCampaignsByStage(
+            @PathVariable String stage,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection) {
+        Pageable pageable = PaginationUtils.createPageableWithSorting(page, size, sortBy, sortDirection);
+        return campaignRetrievalService.getCampaignsByStage(stage, pageable);
     }
 
-    @GetMapping({"/campaignsCategory/{categoryId}"})
-    ResponseEntity<List<CampaignDTO>> getCampaignsByCategory(@PathVariable Long categoryId) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignByCategory(categoryId));
+    @GetMapping({"/category/{categoryId}"})
+    ResponseEntity<List<CampaignDTO>> getCampaignsByCategory(
+            @PathVariable Short categoryId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection) {
+        Pageable pageable = PaginationUtils.createPageableWithSorting(page, size, sortBy, sortDirection);
+        return campaignRetrievalService.getCampaignByCategory(categoryId, pageable);
     }
 
-    @GetMapping({"/campaignsSubCategory/{subCategoryId}"})
-    ResponseEntity<List<CampaignDTO>> getCampaignsBySubCategory(@PathVariable Long subCategoryId) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignBySubCategory(subCategoryId));
+    @GetMapping({"/subCategory/{subCategoryId}"})
+    ResponseEntity<List<CampaignDTO>> getCampaignsBySubCategory(
+            @PathVariable Short subCategoryId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection) {
+        Pageable pageable = PaginationUtils.createPageableWithSorting(page, size, sortBy, sortDirection);
+        return campaignRetrievalService.getCampaignBySubCategory(subCategoryId, pageable);
     }
 
-    @GetMapping({"/campaignsStage/{campaignStage}"})
-    ResponseEntity<List<CampaignDTO>> getCampaignsByStage(@PathVariable String campaignStage) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignsByStage(campaignStage));
+    @GetMapping({"/fundingType/{fundingTypeId}"})
+    ResponseEntity<List<CampaignDTO>> getCampaignsByFundingType(
+            @PathVariable Short fundingTypeId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection) {
+        Pageable pageable = PaginationUtils.createPageableWithSorting(page, size, sortBy, sortDirection);
+        return campaignRetrievalService.getCampaignsByFundingType(fundingTypeId, pageable);
     }
 
-    @GetMapping({"/campaignsFundingType/{fundingTypeId}"})
-    ResponseEntity<List<CampaignDTO>> getCampaignsByFundingType(@PathVariable Long fundingTypeId) {
-        return ResponseEntity.ok(this.campaignRetrievalService.getCampaignsByFundingType(fundingTypeId));
-    }
-
-    @GetMapping({"/searchCampaigns"})
+    @GetMapping("/search")
     ResponseEntity<List<CampaignDTO>> searchCampaigns(@RequestParam String searchParam) {
-        return ResponseEntity.ok(this.campaignRetrievalService.searchCampaigns(searchParam));
+        return ResponseEntity.ok(campaignRetrievalService.searchCampaigns(searchParam));
     }
 
     @PutMapping({"suspend-resume/{campaignId}"})
     ResponseEntity<?> suspendResumeCampaign(@PathVariable Long campaignId, @RequestParam String action) {
         CampaignDTO campaignDTO;
         if (action.equalsIgnoreCase("SUSPEND")) {
-            campaignDTO = this.adminCampaignService.suspendCampaign(campaignId);
+            campaignDTO = adminCampaignService.suspendCampaign(campaignId);
             return ResponseEntity.ok(campaignDTO);
         } else if (action.equalsIgnoreCase("RESUME")) {
-            campaignDTO = this.adminCampaignService.resumeCampaign(campaignId);
+            campaignDTO = adminCampaignService.resumeCampaign(campaignId);
             return ResponseEntity.ok(campaignDTO);
         } else {
             return ApiResponse.error(HttpStatus.BAD_REQUEST, "Invalid action. Action should be either 'SUSPEND' or 'RESUME'.");
         }
     }
 
-    @DeleteMapping({"delete/{campaignId}"})
-    ResponseEntity<ApiResponse> deleteCampaign(@PathVariable Long campaignId) {
-        this.campaignOperationService.deleteCampaign(campaignId);
-        return ApiResponse.success("Campaign successfully deleted!");
+    @GetMapping({"/approvals/{campaignId}"})
+    public ResponseEntity<ApprovalResponse> getCampaignApprovalByCampaignId(@PathVariable Long campaignId) {
+        return ResponseEntity.ok(adminCampaignApprovalService.getCampaignApprovalByCampaignId(campaignId));
+    }
+
+    @PostMapping({"/approvals"})
+    public ResponseEntity<String> approveCampaign(@ModelAttribute @Valid CampaignApprovalReq campaignApprovalReq) {
+        return adminCampaignApprovalService.approveCampaign(campaignApprovalReq);
+    }
+
+    @DeleteMapping({"/{id}"})
+    ResponseEntity<ApiResponse> deleteCampaign(@PathVariable Long id) {
+        return campaignOperationService.deleteCampaign(id);
     }
 }
